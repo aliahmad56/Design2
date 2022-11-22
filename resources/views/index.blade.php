193997a34e772c49623f8c6d0f01c5086235b5a0
aliahmad56 <aliahmad56b@gmail.com>
2022-11-23 01:06:00 +0500

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,402 @@
+<!doctype html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- Bootstrap CSS -->
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-EVSTQN3/azprG1Anm3QDgpJLIm9Nao0Yz1ztcQTwFspd3yD65VohhpuuCOmLASjC" crossorigin="anonymous">
+    <link rel="stylesheet" href="{{asset ('Css/custom.css')}}"/>
+    <script src='https://kit.fontawesome.com/a076d05399.js' crossorigin='anonymous'></script>
+    <title>Hello, world!</title>
+  </head>
+  <body>
+   <!-- ==  Header === -->
+
+   <div class="container-fluid">
+    <header class="d-flex justify-content-center py-3 mt-3 border-bottom">
+      <ul class="nav nav-pills">
+          <li><img src="{{asset ('images/CTW-logo.png')}}" class="logoimage " style="width: 120px;"/></li>
+        <li class="nav-item mt-3 px-2"><a href="#" class="nav-link" aria-current="page">Cloud</a></li>
+        <li class="nav-item mt-3 px-2"><a href="#" class="nav-link">Features</a></li>
+        <li class="nav-item mt-3 px-2"><a href="#" class="nav-link">Managed</a></li>
+        <li class="nav-item mt-3 px-2"><a href="#" class="nav-link">Colocation</a></li>
+        <li class="nav-item mt-3 px-2"><a href="#" class="nav-link">Connectivity</a></li>
+        <li class="nav-item mt-3 px-2"><a href="#" class="nav-link">Location</a></li>
+        <li class="nav-item mt-3 px-2"><a href="#" class="nav-link">0114 123 45678</a></li>
+        <button type="button" class="btn px-2" disabled style="background-color: #1E1E53; color:white; height:50px; margin-top:10px">Get in touch</button>
+
+      </ul>
+    </header>
+  </div>
+   <!-- === Header End === -->
+
+
+   <!-- === Section 1 === -->
+
+   <section class="main">
+<div class="container">
+<div class="row">
+<div class="col-6 col-sm-6 col-lg-6 col-xl-6 col-xxl-6">
+    <div class="section1_ist_col_content">
+<p>Welcome to CTW</p>
+<h1 style="color: #1E1E53; font-weight:800; font-family: Arial;">ALWAYS ON IT.</h1>
+<b>The Right Solution For Your Technological Problems</b></br></br>
+
+<button type="button" class="btn btn-lg section1button" disabled style="background-color: #1E1E53;">Get Quote Now</button>
+<button type="button" class="btn btn-lg section1button" disabled style="background-color: #1E1E53;">See Pricing</button>
+</div>
+
+
+</div>
+
+<div class="col-6 col-sm-6 col-lg-6 col-xl-6 col-xxl-6">
+
+<button type="button" class="rounded-circle btn btn-lg vediobtn" disabled></button>
+
+
+</div>
+
+</div>
+</div>
+</section>
+
+
+   <!-- === Section 1 End === -->
+
+
+
+   <!-- ==Section 2 Started== -->
+   <section class="main">
+<div class="container" style="margin-top:">
+<div class="row mt-5">
+
+<div class="col-6 col-sm-6 col-lg-6 col-xl-6 col-xxl-6">
+<img src="{{asset ('images/Section2 image.png')}}">
+</div>
+
+<div class="col-6 col-sm-6 col-lg-6 col-xl-6 col-xxl-6">
+<p>History</p>
+<h1 style="color: #1E1E53; font-weight:800; font-family: Arial;">About CTW</h1>
+<p class="Paragrah1">CTW is a leading IT services company known for its exceptional service and response times. 
+We provide cloud, hosting, colocation, infrastructure fully managed services as well as remote working, 
+to businesses across the globe. If you’re looking for secure, robust, high quality IT and hosting solutions.</p>
+<h6 id="readmore"> Read more </h6> 
+<i style="font-size:24px; width: 100px;" class='fas'>&#xf061;</i>
+</div>
+
+</div>
+</div>
+</section>
+
+   <!-- ==Section 2 Ended ==  -->
+
+   <!-- ==Section 3 Started == -->
+   <section class="main">
+<div class="container" style="margin-top:">
+<center> <h1 style="color: #1E1E53; font-weight:800; font-family: Arial; margin-top: 120px; margin-bottom: 120px;">What we provide</h1></center>
+
+
+<div class="row mt-5">
+<div class="col-4 col-sm-4 col-lg-4 col-xl-4 col-xxl-4">
+</br>
+</br>
+<!-- Card 1 -->
+<div class="card" style="width: 18rem;">
+<center> <img src="{{asset('images/Capture1.PNG')}}" class="card-img-top" alt="..."></center>
+  <div class="card-body">
+  <center><h1 style="color: #1E1E53; font-weight:800; font-family: Arial;">Control</h1></center>
+   <center><p class="card-text"style="margin-top: 30px; margin-bottom: 40px; font-weight:600">As these resources are not shared with others you have much more control.</p></center>
+  </div>
+</div>
+</div>  
+<!-- Card End -->
+
+<!-- Card 2 -->
+<div class="col-4 col-sm-4 col-lg-4 col-xl-4 col-xxl-4">
+</br>
+</br>
+
+<div class="card" style="width: 18rem;">
+<center><img src="{{ asset('images/Capture2.PNG')}}" class="card-img-top" alt="..."></center>
+  <div class="card-body">
+  <center><h1 style="color: #1E1E53; font-weight:800; font-family: Arial; margin-top: 30px;">Flexibility</h1></center>
+    <center><p class="card-text" style="margin-top: 20px; margin-bottom: 40px;font-weight:600">With the correct implementation we can build a solution that fits around.</p></center>
+  </div>
+</div>
+</div>
+<!-- End -->
+
+<!-- Card 3 -->
+<div class="col-4 col-sm-4 col-lg-4 col-xl-4 col-xxl-4">
+</br>
+</br>
+
+<div class="card" style="width: 18rem;">
+ <center> <img src="{{asset('images/Capture3.PNG')}}" class="card-img-top" alt="..."></center>
+  <div class="card-body">
+  <center><h1 style="color: #1E1E53; font-weight:800; font-family: Arial; margin-top: 20px;">Scalability</h1></center>
+    <center><p class="card-text" style="font-weight:600;margin-top: 20px; margin-bottom: 40px;">Built for unpredictable traffic private cloud offers easily scalable resources.</p></center>
+  </div>
+</div>
+</div>
+<!-- End -->
+
+
+<div class="row mt-5">
+<div class="col-4 col-sm-4 col-lg-4 col-xl-4 col-xxl-4">
+</br>
+</br>
+<!-- Card 1 -->
+<div class="card" style="width: 18rem;">
+<center> <img src="{{asset('images/Capture1.PNG')}}" class="card-img-top" alt="..."></center>
+  <div class="card-body">
+  <center><h1 style="color: #1E1E53; font-weight:800; font-family: Arial;">Control</h1></center>
+   <center><p class="card-text"style="margin-top: 30px; margin-bottom: 40px; font-weight:600">As these resources are not shared with others you have much more control.</p></center>
+  </div>
+</div>
+</div>  
+<!-- Card End -->
+
+<!-- Card 2 -->
+<div class="col-4 col-sm-4 col-lg-4 col-xl-4 col-xxl-4">
+</br>
+</br>
+
+<div class="card" style="width: 18rem;">
+<center><img src="{{ asset('images/Capture2.PNG')}}" class="card-img-top" alt="..."></center>
+  <div class="card-body">
+  <center><h1 style="color: #1E1E53; font-weight:800; font-family: Arial; margin-top: 30px;">Flexibility</h1></center>
+    <center><p class="card-text" style="margin-top: 20px; margin-bottom: 40px;font-weight:600">With the correct implementation we can build a solution that fits around.</p></center>
+  </div>
+</div>
+</div>
+<!-- End -->
+
+<!-- Card 3 -->
+<div class="col-4 col-sm-4 col-lg-4 col-xl-4 col-xxl-4">
+</br>
+</br>
+
+<div class="card" style="width: 18rem;">
+ <center> <img src="{{asset('images/Capture3.PNG')}}" class="card-img-top" alt="..."></center>
+  <div class="card-body">
+  <center><h1 style="color: #1E1E53; font-weight:800; font-family: Arial; margin-top: 20px;">Scalability</h1></center>
+    <center><p class="card-text" style="font-weight:600;margin-top: 20px; margin-bottom: 40px;">Built for unpredictable traffic private cloud offers easily scalable resources.</p></center>
+  </div>
+</div>
+</div>
+<!-- End -->
+</div>
+</div>
+</section>
+   <!-- ==Section 3 Ended == -->
+ 
+   <section class="main">
+<div class="container-fluid" style="margin-top:">
+<div class="row mt-5">
+<div class="col-12 col-sm-12 col-lg-12 col-xl-12 col-xxl-12">
+<img src="{{asset ('images/ddos-bg.png')}}" class="logoimage " style="width:100%; height: 70%;"/>
+
+</div>
+</div>
+</div>
+</section>
+
+<!-- Last Section Start-->
+<center> <h1 style="color: #1E1E53; font-weight:800; font-family: Arial; margin-top: 40px; margin-bottom: 120px;">Talk To an Advisor Today</h1></center>
+<section class="main">
+<div class="container" style="margin-top:">
+
+<div class="row mt-5">
+<div class="col-4 col-sm-4 col-lg-4 col-xl-4 col-xxl-4">
+</br>
+</br>
+<!-- Card 1 -->
+<div class="card" style="width: 18rem;">
+<center> <img src="{{asset('images/Capture1.PNG')}}" class="card-img-top" alt="..."></center>
+  <div class="card-body">
+  <center><h1 style="color: #1E1E53; font-weight:800; font-family: Arial;">Start a live chat</h1></center>
+   <center><p class="card-text"style="margin-top: 30px; margin-bottom: 40px; font-weight:600">As these resources are not shared with others you have much more control.</p></center>
+  </div>
+</div>
+</div>  
+<!-- Card End -->
+
+<!-- Card 2 -->
+<div class="col-4 col-sm-4 col-lg-4 col-xl-4 col-xxl-4">
+</br>
+</br>
+
+<div class="card" style="width: 18rem;">
+<center><img src="{{ asset('images/Capture2.PNG')}}" class="card-img-top" alt="..."></center>
+  <div class="card-body">
+  <center><h1 style="color: #1E1E53; font-weight:800; font-family: Arial; margin-top: 30px;">Call us at</h1></center>
+    <center><p class="card-text" style="margin-top: 20px; margin-bottom: 40px;font-weight:600">With the correct implementation we can build a solution that fits around.</p></center>
+  </div>
+</div>
+</div>
+<!-- End -->
+
+<!-- Card 3 -->
+<div class="col-4 col-sm-4 col-lg-4 col-xl-4 col-xxl-4">
+</br>
+</br>
+
+<div class="card" style="width: 18rem;">
+ <center> <img src="{{asset('images/Capture3.PNG')}}" class="card-img-top" alt="..."></center>
+  <div class="card-body">
+  <center><h1 style="color: #1E1E53; font-weight:800; font-family: Arial; margin-top: 20px;">Here we are</h1></center>
+    <center><p class="card-text" style="font-weight:600;margin-top: 20px; margin-bottom: 40px;">Built for unpredictable traffic private cloud offers easily scalable resources.</p></center>
+  </div>
+</div>
+</div>
+
+</div>
+</section>
+<!-- End -->
+
+
+<!-- Last Section End -->
+
+<!-- == Footer Started == -->
+
+
+<footer class="text-center text-white" style="background: #091B29;">
+  <!-- Grid container -->
+  <div class="container p-4">
+    
+
+    <!-- Section: Form -->
+    <section class="">
+      <form action="">
+        <!--Grid row-->
+        <div class="row d-flex justify-content-left">
+        
+
+          <!--Grid column-->
+          <div class="col-md-5 col-12">
+            <!-- Email input -->
+            <div class="form-outline form-white mb-4">
+              <input type="email" id="form5Example21" class="form-control" style="height: 60px;"/>
+            
+            </div>
+          </div>
+          <!--Grid column-->
+
+          <!--Grid column-->
+          <div class="col-auto">
+            <!-- Submit button -->
+            <button type="submit" class="btn btn-outline-light mb-4" style="height: 60px; width: 160px;">
+              Subscribe
+            </button>
+          </div>
+          <!--Grid column-->
+        </div>
+        <!--Grid row-->
+      </form>
+    </section>
+    <!-- Section: Form -->
+    <!-- Section: Links -->
+    <section class="">
+      <!--Grid row-->
+        <!--Grid column-->
+        
+        <!--Grid column-->
+     
+
+      <div class="row py-5">
+
+      <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <image src="{{asset ('images/Bootomimage.png')}}"/>
+        </div>
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">Our Servives</h5>
+
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a href="#!" class="text-white">Link 1</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">Link 2</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">Link 3</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">Link 4</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">Quick Links</h5>
+
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a href="#!" class="text-white">Link 1</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">Link 2</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">Link 3</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">Link 4</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+
+        <!--Grid column-->
+        <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+          <h5 class="text-uppercase">Contacts</h5>
+
+          <ul class="list-unstyled mb-0">
+            <li>
+              <a href="#!" class="text-white">Link 1</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">Link 2</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">Link 3</a>
+            </li>
+            <li>
+              <a href="#!" class="text-white">Link 4</a>
+            </li>
+          </ul>
+        </div>
+        <!--Grid column-->
+
+      </div>
+      <!--Grid row-->
+    </section>
+    <!-- Section: Links -->
+  </div>
+  <!-- Grid container -->
+
+
+  <!-- == Footer Start Here  -->
+  <footer>
+
+<div class="footerdiv">
+  All rights reserved by CTW 2021
+  <p class="footerpart2">Terms & Conditions | Privacy Policy | Privacy Manager</p>
+</div>
+
+</footer>
+  <!-- Copyright
+</footer> -->
+
+<!-- Footer Ended -->
+
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.0.2/dist/js/bootstrap.bundle.min.js" integrity="sha384-MrcW6ZMFYlzcLA8Nl+NtUVF0sA7MsXsP1UyJoMp4YLEuNSfAP+JcXn/tWtIaxVXM" crossorigin="anonymous"></script>
+
+  </body>
+</html>
